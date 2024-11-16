b79d4ce90447fcd7c8a87ce847b9a86de64964a6
Peeranut-Kit <peeranut.kit.work@gmail.com>
2024-11-17 02:51:23 +0700

    Fix DateOfBirth datatype

--- internal/patient/repository.go
+++ internal/patient/repository.go
@@ -25,54 +25,54 @@ func (r *GormPatientRepository) SearchPatient(request *pkg.Patient) ([]pkg.Patie
 
 	query := r.db.Table("patients").Where("hospital_id = ?", request.HospitalID)
 
-    // Add optional conditions only if fields are populated
-    if request.ID != 0 {
-        query = query.Where("id = ?", request.ID)
-    }
-    if request.FirstNameTh != "" {
-        query = query.Where("first_name_th = ?", request.FirstNameTh)
-    }
-    if request.MiddleNameTh != "" {
-        query = query.Where("middle_name_th = ?", request.MiddleNameTh)
-    }
-    if request.LastNameTh != "" {
-        query = query.Where("last_name_th = ?", request.LastNameTh)
-    }
-    if request.FirstNameEn != "" {
-        query = query.Where("first_name_en = ?", request.FirstNameEn)
-    }
-    if request.MiddleNameEn != "" {
-        query = query.Where("middle_name_en = ?", request.MiddleNameEn)
-    }
-    if request.LastNameEn != "" {
-        query = query.Where("last_name_en = ?", request.LastNameEn)
-    }
-    if request.DateOfBirth != nil {
-        query = query.Where("date_of_birth = ?", *request.DateOfBirth)
-    }
-    if request.PatientHN != "" {
-        query = query.Where("patient_hn = ?", request.PatientHN)
-    }
-    if request.NationalID != "" {
-        query = query.Where("national_id = ?", request.NationalID)
-    }
-    if request.PassportID != "" {
-        query = query.Where("passport_id = ?", request.PassportID)
-    }
-    if request.PhoneNumber != "" {
-        query = query.Where("phone_number = ?", request.PhoneNumber)
-    }
-    if request.Email != "" {
-        query = query.Where("email = ?", request.Email)
-    }
-    if request.Gender != "" {
-        query = query.Where("gender = ?", request.Gender)
-    }
+	// Add optional conditions only if fields are populated
+	if request.ID != 0 {
+		query = query.Where("id = ?", request.ID)
+	}
+	if request.FirstNameTh != "" {
+		query = query.Where("first_name_th = ?", request.FirstNameTh)
+	}
+	if request.MiddleNameTh != "" {
+		query = query.Where("middle_name_th = ?", request.MiddleNameTh)
+	}
+	if request.LastNameTh != "" {
+		query = query.Where("last_name_th = ?", request.LastNameTh)
+	}
+	if request.FirstNameEn != "" {
+		query = query.Where("first_name_en = ?", request.FirstNameEn)
+	}
+	if request.MiddleNameEn != "" {
+		query = query.Where("middle_name_en = ?", request.MiddleNameEn)
+	}
+	if request.LastNameEn != "" {
+		query = query.Where("last_name_en = ?", request.LastNameEn)
+	}
+	if !request.DateOfBirth.IsZero() {
+		query = query.Where("date_of_birth = ?", request.DateOfBirth)
+	}
+	if request.PatientHN != "" {
+		query = query.Where("patient_hn = ?", request.PatientHN)
+	}
+	if request.NationalID != "" {
+		query = query.Where("national_id = ?", request.NationalID)
+	}
+	if request.PassportID != "" {
+		query = query.Where("passport_id = ?", request.PassportID)
+	}
+	if request.PhoneNumber != "" {
+		query = query.Where("phone_number = ?", request.PhoneNumber)
+	}
+	if request.Email != "" {
+		query = query.Where("email = ?", request.Email)
+	}
+	if request.Gender != "" {
+		query = query.Where("gender = ?", request.Gender)
+	}
 
 	// Execute the query
 	if err := query.Find(&patientList).Error; err != nil {
 		return nil, err
 	}
-    
+
 	return patientList, nil
 }
--- pkg/model.go
+++ pkg/model.go
@@ -12,22 +12,22 @@ type Hospital struct {
 }
 
 type Patient struct {
-	ID           int        `gorm:"primaryKey" json:"id"`
-	FirstNameTh  string     `gorm:"size:255" json:"first_name_th"`
-	MiddleNameTh string     `gorm:"size:255" json:"middle_name_th"`
-	LastNameTh   string     `gorm:"size:255" json:"last_name_th"`
-	FirstNameEn  string     `gorm:"size:255" json:"first_name_en"`
-	MiddleNameEn string     `gorm:"size:255" json:"middle_name_en"`
-	LastNameEn   string     `gorm:"size:255" json:"last_name_en"`
-	DateOfBirth  *time.Time `json:"date_of_birth"`
-	PatientHN    string     `gorm:"size:50;not null;unique" json:"patient_hn"`
-	NationalID   string     `gorm:"size:50;not null;unique" json:"national_id"`
-	PassportID   string     `gorm:"size:50;not null;unique" json:"passport_id"`
-	PhoneNumber  string     `gorm:"size:50;not null" json:"phone_number"`
-	Email        string     `gorm:"size:255;unique" json:"email"`
-	Gender       string     `gorm:"size:1" json:"gender"`
-	HospitalID   int        `json:"hospital_id"`
-	Hospital     Hospital   `gorm:"foreignKey:HospitalID" json:"hospital"`
+	ID           int       `gorm:"primaryKey" json:"id"`
+	FirstNameTh  string    `gorm:"size:255" json:"first_name_th"`
+	MiddleNameTh string    `gorm:"size:255" json:"middle_name_th"`
+	LastNameTh   string    `gorm:"size:255" json:"last_name_th"`
+	FirstNameEn  string    `gorm:"size:255" json:"first_name_en"`
+	MiddleNameEn string    `gorm:"size:255" json:"middle_name_en"`
+	LastNameEn   string    `gorm:"size:255" json:"last_name_en"`
+	DateOfBirth  time.Time `json:"date_of_birth"`
+	PatientHN    string    `gorm:"size:50;not null;unique" json:"patient_hn"`
+	NationalID   string    `gorm:"size:50;not null;unique" json:"national_id"`
+	PassportID   string    `gorm:"size:50;not null;unique" json:"passport_id"`
+	PhoneNumber  string    `gorm:"size:50;not null" json:"phone_number"`
+	Email        string    `gorm:"size:255;unique" json:"email"`
+	Gender       string    `gorm:"size:1" json:"gender"`
+	HospitalID   int       `json:"hospital_id"`
+	Hospital     Hospital  `gorm:"foreignKey:HospitalID" json:"hospital"`
 }
 
 type Staff struct {
